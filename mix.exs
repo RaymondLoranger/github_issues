afb9207a3c60a2d41e60cea9a20ff1751b39983f
RaymondLoranger <Raymond.Loranger@bell.net>
2018-02-03 13:54:08 -0500

    mix format

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GitHub.Issues.Mixfile do
   def project do
     [
       app: :github_issues,
-      version: "0.3.20",
+      version: "0.3.21",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       name: "GitHub Issues",
