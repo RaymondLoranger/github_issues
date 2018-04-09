054881bb285f281e3157549311ff105e714b6a34
RaymondLoranger <Raymond.Loranger@bell.net>
2018-04-09 10:10:59 -0400

    0.4.11

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GitHub.Issues.Mixfile do
   def project do
     [
       app: :github_issues,
-      version: "0.4.10",
+      version: "0.4.11",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       name: "GitHub Issues",
