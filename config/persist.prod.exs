c457fbc598d4bb16c73f3b34a5e54ad946a25233
RaymondLoranger <Raymond.Loranger@bell.net>
2018-12-21 06:54:12 -0500

    0.4.28

---
+++ config/persist.prod.exs
@@ -0,0 +1,31 @@
+use Mix.Config
+
+config :github_issues, default_count: 7
+
+config :io_ansi_table,
+  align_specs: [
+    right: "number"
+  ]
+
+config :io_ansi_table,
+  headers: [
+    "number",
+    "created_at",
+    "updated_at",
+    # "id",
+    "title",
+    "state"
+  ]
+
+config :io_ansi_table,
+  header_fixes: %{
+    # ~r[^id$]i => "ID"
+    ~r[ at$]i => " at"
+  }
+
+config :io_ansi_table,
+  sort_specs: [
+    "created_at"
+  ]
+
+config :log_reset, reset?: true
